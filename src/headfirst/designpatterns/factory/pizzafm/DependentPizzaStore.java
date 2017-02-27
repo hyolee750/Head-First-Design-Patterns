ab66fa8a388ff817e84b87b9b3627c282e3b17d6
hyolee <815366375@qq.com>
2017-02-27 10:10:05 +0800

    head first

--- src/headfirst/designpatterns/factory/pizzafm/DependentPizzaStore.java
+++ src/headfirst/designpatterns/factory/pizzafm/DependentPizzaStore.java
@@ -4,29 +4,42 @@ public class DependentPizzaStore {
  
 	public Pizza createPizza(String style, String type) {
 		Pizza pizza = null;
-		if (style.equals("NY")) {
-			if (type.equals("cheese")) {
-				pizza = new NYStyleCheesePizza();
-			} else if (type.equals("veggie")) {
-				pizza = new NYStyleVeggiePizza();
-			} else if (type.equals("clam")) {
-				pizza = new NYStyleClamPizza();
-			} else if (type.equals("pepperoni")) {
-				pizza = new NYStylePepperoniPizza();
-			}
-		} else if (style.equals("Chicago")) {
-			if (type.equals("cheese")) {
-				pizza = new ChicagoStyleCheesePizza();
-			} else if (type.equals("veggie")) {
-				pizza = new ChicagoStyleVeggiePizza();
-			} else if (type.equals("clam")) {
-				pizza = new ChicagoStyleClamPizza();
-			} else if (type.equals("pepperoni")) {
-				pizza = new ChicagoStylePepperoniPizza();
-			}
-		} else {
-			System.out.println("Error: invalid type of pizza");
-			return null;
+		switch (style) {
+			case "NY":
+				switch (type) {
+					case "cheese":
+						pizza = new NYStyleCheesePizza();
+						break;
+					case "veggie":
+						pizza = new NYStyleVeggiePizza();
+						break;
+					case "clam":
+						pizza = new NYStyleClamPizza();
+						break;
+					case "pepperoni":
+						pizza = new NYStylePepperoniPizza();
+						break;
+				}
+				break;
+			case "Chicago":
+				switch (type) {
+					case "cheese":
+						pizza = new ChicagoStyleCheesePizza();
+						break;
+					case "veggie":
+						pizza = new ChicagoStyleVeggiePizza();
+						break;
+					case "clam":
+						pizza = new ChicagoStyleClamPizza();
+						break;
+					case "pepperoni":
+						pizza = new ChicagoStylePepperoniPizza();
+						break;
+				}
+				break;
+			default:
+				System.out.println("Error: invalid type of pizza");
+				return null;
 		}
 		pizza.prepare();
 		pizza.bake();
